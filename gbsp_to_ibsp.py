44a0e35a45fa96185bce0106cf0ce9bf4f915f60
Frank Vollebregt <fcjvollebregt@gmail.com>
2022-05-05 12:58:44 +0200

    Refactor main file with better printing

--- gbsp_to_ibsp.py
+++ gbsp_to_ibsp.py
@@ -1,8 +1,9 @@
-from bsp import GBSPChunk
+from gbsp import GBSPChunk
 from struct import pack
-from main_quake import IBSP_TYPES
-from full_test import convert_to_ibsp_full
+from read_ibsp import IBSP_TYPES
+from convert import convert_to_ibsp
 import sys
+
 
 # Read a GBSP header and bytes, and put them into a GBSPChunk object
 def read_gbsp_chunk(f):
@@ -27,25 +28,31 @@ def read_gbsp_file(path):
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print('Usage: python gbsp_to_ibsp.py [filename.bsp]')
+        print('Usage: python gbsp_to_ibsp.py [filename.bsp] [(optional: output_filename.bsp)]')
+        exit(-1)
 
     path = sys.argv[1]
     gbsp = read_gbsp_file(path)
 
     # convert GBSP data to the correct IBSP data
-    ibsp_data = convert_to_ibsp_full(gbsp)
+    ibsp_data = convert_to_ibsp(gbsp)
 
-    print("".join([key + ', ' for key, value in ibsp_data.items()]))
+    print("IBSP lumps to write: " + "".join([key + ', ' for key, value in ibsp_data.items()]))
 
-    out_file = open('bsp/test_quake.bsp', 'w+b')
+    out_path = path.split('.')[0] + '_ibsp.' + path.split('.')[1]
+    if len(sys.argv) >= 3:
+        out_path = sys.argv[2]
+
+    out_file = open(out_path, 'w+b')
+    print('writing results to {}'.format(out_path))
 
     # first, write the header
     out_file.write(bytearray('IBSP', 'utf8'))            # identifier
     out_file.write(pack("<I", 38))     # version
 
     # then, write the lumps
-    # keep track of where we can put the next one
-    pointer = 160
+    # keep track of the pointer where we can put the next one
+    pointer = 160   # start after the header, which is always 160 bytes
     bytes_to_write = []
     for i in range(19):
         if IBSP_TYPES[i] == 'IBSP_PLANES':
@@ -53,48 +60,42 @@ if __name__ == '__main__':
             out_file.write(pack("<II", pointer, planes['size'] * planes['elements']))
             pointer += planes['size'] * planes['elements']
             bytes_to_write.append(planes['bytes'])
-            print("wrote {} bytes for planes".format(len(planes['bytes'])))
-            print('length matches: {}'.format(len(planes['bytes']) == planes['size']*planes['elements']))
+            print("wrote {} planes ({} bytes)".format(planes['elements'], len(planes['bytes'])))
         elif IBSP_TYPES[i] == 'IBSP_VERTICES':
             verts = ibsp_data['vertices']
             out_file.write(pack("<II", pointer, verts['size'] * verts['elements']))
             pointer += verts['size'] * verts['elements']
             bytes_to_write.append(verts['bytes'])
-            print("wrote {} bytes for verts".format(len(verts['bytes'])))
-            print('length matches: {}'.format(len(verts['bytes']) == verts['size'] * verts['elements']))
+            print("wrote {} verts ({} bytes)".format(verts['elements'], len(verts['bytes'])))
         elif IBSP_TYPES[i] == 'IBSP_FACES':
             faces = ibsp_data['faces']
             out_file.write(pack("<II", pointer, faces['size'] * faces['elements']))
             pointer += faces['size'] * faces['elements']
             bytes_to_write.append(faces['bytes'])
-            print("wrote {} bytes for faces".format(len(faces['bytes'])))
-            print('length matches: {}'.format(len(faces['bytes']) == faces['size'] * faces['elements']))
+            print("wrote {} faces ({} bytes)".format(faces['elements'], len(faces['bytes'])))
         elif IBSP_TYPES[i] == 'IBSP_EDGES':
             edges = ibsp_data['edges']
             out_file.write(pack("<II", pointer, edges['size'] * edges['elements']))
             pointer += edges['size'] * edges['elements']
             bytes_to_write.append(edges['bytes'])
-            print("wrote {} bytes for edges".format(len(edges['bytes'])))
-            print('length matches: {}'.format(len(edges['bytes']) == edges['size'] * edges['elements']))
+            print("wrote {} edges ({} bytes)".format(edges['elements'], len(edges['bytes'])))
         elif IBSP_TYPES[i] == 'IBSP_FACE_EDGE_TABLE':
             face_edges = ibsp_data['face_edges']
             out_file.write(pack("<II", pointer, face_edges['size'] * face_edges['elements']))
             pointer += face_edges['size'] * face_edges['elements']
             bytes_to_write.append(face_edges['bytes'])
-            print("wrote {} bytes for face_edges".format(len(face_edges['bytes'])))
-            print('length matches: {}'.format(len(face_edges['bytes']) == face_edges['size'] * face_edges['elements']))
+            print("wrote {} face_edges ({} bytes)".format(face_edges['elements'], len(face_edges['bytes'])))
         elif IBSP_TYPES[i] == 'IBSP_TEXTURE_INFORMATION':
             texture_info = ibsp_data['texture_info']
             out_file.write(pack("<II", pointer, texture_info['size'] * texture_info['elements']))
             pointer += texture_info['size'] * texture_info['elements']
             bytes_to_write.append(texture_info['bytes'])
-            print("wrote {} bytes for texture_info".format(len(texture_info['bytes'])))
-            print('length matches: {}'.format(len(texture_info['bytes']) == texture_info['size'] * texture_info['elements']))
+            print("wrote {} texture_info ({} bytes)".format(texture_info['elements'], len(texture_info['bytes'])))
         else:
-            # don't include this, keep offset and length 0
+            # don't include this lump, set offset and length 0
             out_file.write(pack("<II", 0, 0))
 
-    # write plane
+    # write all the lump data
     for byte_group in bytes_to_write:
         out_file.write(byte_group)
 
